9b41f8a381147e1093e68ba371495a143bff7667
jydidier <jean-yves.didier@ibisc.univ-evry.fr>
2025-11-30 22:43:05 +0100

    added a few things for ui

--- scripts/background.js
+++ scripts/background.js
@@ -14,6 +14,21 @@ let timetrackerMenuItem = await browser.menus.create({
     }
 );
 
+let createBoard = async () => {
+    let tab = await browser.tabs.query({ 
+        "cookieStoreId": timetrackerContext.cookieStoreId,
+    });
+
+    if (tab.length ===0) {
+        browser.tabs.create({
+            "cookieStoreId": timetrackerContext.cookieStoreId,
+            "url": "./ui/board.html"
+        });
+    } else {
+        browser.tabs.update(tab[0].id, { active: true });
+    }
+};
+
 
 if (timetrackerMenuItem === null) {
     console.log(browser.runtime.lastError);
@@ -23,8 +38,10 @@ if (timetrackerMenuItem === null) {
 
 browser.menus.onClicked.addListener(async (info, tab) => {
     if (info.menuItemId === timetrackerMenuItem) {
+        createBoard();
     }
 });
 
 browser.action.onClicked.addListener(async (...args) => {
+    createBoard();
 });
---
+++ scripts/timetracker.js
@@ -0,0 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+/** TODO Organize extension in a more modular way */
+
+import './i18n.js';
+
+import * as JCAL from './jcal.js';
+import {marked} from '../vendor/marked.esm.js';
