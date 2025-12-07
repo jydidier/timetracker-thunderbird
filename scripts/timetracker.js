30f385b11351da749065ad4ade9651246317b9d0
jydidier <jean-yves.didier@ibisc.univ-evry.fr>
2025-12-08 00:15:42 +0100

    task are now loaded from calendars

--- scripts/timetracker.js
+++ scripts/timetracker.js
@@ -11,6 +11,18 @@ import './i18n.js';
 import * as JCAL from './jcal.js';
 import {marked} from '../vendor/marked.esm.js';
 
+let mc = (globalThis.messenger !== undefined) ?
+    messenger.calendar:(await import('./calendar_front.js'));
+
+
+let capability = {};
+let calendarId = null;
+let updateFrequency = -1;
+
+let tasks = new Map();
+let taskTree = new Map();
+let interval = null;
+
 
 
 /* helper functions */
@@ -46,6 +58,81 @@ const asTodo = (item) => {
 };
 
 
+/* Modal setup for setting */
+const settingsModal = document.getElementById('settingsModal');
+const saveSettings = document.getElementById('saveSettings');
+
+
+
+let populateCalendars = (calendars, filterList) => {
+    filterList.textContent = "";
+    calendars.sort( (a,b) => a.name.localeCompare(b.name) ).forEach(calendar => {
+        let filterItem = document.createElement('div');
+        filterItem.classList.add('form-check');
+        filterItem.insertAdjacentHTML(
+            'beforeend',
+            DOMPurify.sanitize(`
+                <input class="form-check-input" type="radio" value="${calendar.id}" name="calendar" id="filter-${calendar.id}" ${(calendarId && calendarId === calendar.id)?'checked':''}>
+                <label class="form-check-label" for="filter-${calendar.id}">
+                    <i class="bi bi-circle-fill" style="color:${calendar.color}"></i>
+                    ${calendar.name}
+                </label>
+            `)
+        );
+        filterList.appendChild(filterItem);
+    });
+};
+
+if (settingsModal) {
+    settingsModal.addEventListener('show.bs.modal', async (event) => {
+        let calendars = await mc.calendars.query(capability);
+        let calList = document.getElementById('calendarList');
+        let calendarCapability = document.getElementById('calendarCapability');
+        let updateFrequencyField = document.getElementById('updateFrequency');
+
+        updateFrequencyField.value = updateFrequency;
+
+        populateCalendars(calendars, calList); 
+        
+        if (capability.tasksSupported) {
+            calendarCapability.checked = true;
+        } else {
+            calendarCapability.checked = false;
+        }
+
+        calendarCapability.addEventListener('change', async (event) => {
+            capability = event.target.checked ? {tasksSupported: true} : {};
+            let calendars = await mc.calendars.query(capability);
+            populateCalendars(calendars, calList);
+        });
+    });
+}
+
+
+if (saveSettings) {
+    saveSettings.addEventListener('click', async (event) => {
+        let calendarList = document.getElementById('calendarList');
+        let calendarIds = [];
+        calendarIds = Array.from(calendarList.querySelectorAll('input:checked')).map(input => input.value);
+        calendarId = (calendarIds.length > 0)?calendarIds[0]:null;
+
+        let updateFrequencyField = document.getElementById('updateFrequency');
+        updateFrequencyField.value = updateFrequency;
+
+        // in case of changes, we should 
+        // 1/ stop all current task
+        // 2/ reload all
+
+        setStorage("timetracker-update-frequency", updateFrequency);
+        setStorage("timetracker-calendar", calendarId);
+
+        let modal = bootstrap.Modal.getInstance(document.getElementById("settingsModal"));
+        modal.hide();    
+    });
+}
+
+
+
 /* Modal setup for task edition */
 const taskModal = document.getElementById('taskModal');
 const saveTaskButton = document.getElementById('saveTask');
@@ -63,6 +150,7 @@ const saveTask = async (evt) => {
     let title = document.getElementById('taskTitle').value;
     let description = document.getElementById('taskDescription').value;
 
+    let id = "null";
     let item = (id !== "null") ?
         asTodo(await mc.items.get(calendarId, id, { returnFormat: "jcal" })) :
         new JCAL.Todo();
@@ -74,17 +162,145 @@ const saveTask = async (evt) => {
     if (id !== "null") {
         item.uid = id;
         await mc.items.update(calendarId, id, {format: 'jcal', item: item.data});
-
-        if (calendarId !== newCalendarId) {
-            await mc.items.move(calendarId, newCalendarId, id);
-        }
     } else {
         item.type="task";
-        await mc.items.create(newCalendarId, {type: 'task', format: 'jcal', item: item.data});
+        await mc.items.create(calendarId, {type: 'task', format: 'jcal', item: item.data});
     }
     let modal = bootstrap.Modal.getInstance(document.getElementById("taskModal"));
     modal.hide();
+};
+
+saveTaskButton.addEventListener('click', saveTask);
+
+
+/* board display */
+let getElapsedTime  = async(task) => {
+    //item.due = (new Date(dueDate)).toISOString(); 
+    let duration = 0;
+
+    task.timeSlices.forEach((item) => {
+        duration += new Date(item.due) - new Date(item.dtstart);
+    });
+    
+    task.children.forEach((item) => {
+        duration += getElapsedTime(item);
+    });
+
+    return duration;
+};
+
+let startStopEvent = async(evt) => {
+    let source = evt.target;
+    console.log(source);
+    let id = source.dataset.event;
+
+    source.classList.toggle("bi-play-fill");
+    source.classList.toggle("bi-stop-fill");
+
+    evt.preventDefault();
+};
+
+let addSubTask = async(evt) => {
+
+    evt.preventDefault();
+};
+
+let deleteTask = async(evt) => {
+
+
+    evt.preventDefault();
 }
 
 
 
+let processMap = async(map, elt) => {
+    map.forEach((v,k) => {
+        let details = document.createElement("ul");
+        details.classList.add('list-group');
+        let summary = document.createElement("li");
+        summary.classList.add('list-group-item','d-flex','justify-content-between','align-items-start');
+
+        details.id = `details-${k}`;
+        summary.insertAdjacentHTML("beforeend",`
+                    ${v.summary}
+
+                    <span>
+                    <a class="bi-play-fill data-event="${k}" id="play-${k}"></a>
+                    <a class="bi-trash3-fill" data-event="${k}" id="delete-${k}"></a>
+                    <a class="bi-plus-lg" data-event="${k}" id="add-${k}"></a>
+                    </span>
+        `
+        );
+        details.appendChild(summary);
+        summary.querySelector(`#play-${k}`).addEventListener("click",startStopEvent);
+
+
+
+        if (v.children.size > 0) {
+            processMap(v.children, details);
+        }
+        elt.appendChild(details);
+    });
+};
+
+let updateBoard = async() => {
+    let taskList = document.getElementById("taskList");
+    taskList.textContent = '';
+    processMap(taskTree, taskList);
+};
+
+/* task loading */
+
+let populateTasks = async () => {
+    if (calendarId === null) return;
+    let items = await mc.items.query({
+        type: "task", 
+        returnFormat: "jcal",
+        calendarId
+    });
+    let todoStack = [];
+    console.log(items);
+
+    items.forEach(item => {
+        let todo = asTodo(item);
+        console.log(todo);
+        todo.children = new Map();
+        todo.timeSlices = new Map();
+
+        tasks.set(todo.uid, todo);
+
+        if (todo.relatedTo) {
+            todoStack.push(todo);
+        } else {
+            taskTree.set(todo.uid, todo);
+        }
+
+        todoStack.forEach(elt => {
+            if (elt.has(elt.relatedTo)) {
+                if (elt.status === 'NEEDS-ACTION') {
+                    tasks.get(elt.relateTo).children.set(elt.uid, elt);
+                } else {
+                    tasks.get(elt.relatedTo).timeSlice.set(elt.uid, elt);
+                }
+            }        
+        });
+    });
+
+
+};
+
+updateFrequency = await getStorage("timetracker-update-frequency") ?? {};
+calendarId = await getStorage("timetracker-calendar") ?? {};
+
+await populateTasks();
+updateBoard();
+
+if (globalThis.messenger !== undefined) {
+    mc.items.onCreated.addListener(populateTasks);
+    mc.items.onUpdated.addListener(populateTasks);
+    mc.items.onRemoved.addListener(populateTasks);
+} else {
+    mc.items.addEventListener("created",populateTasks);
+    mc.items.addEventListener("updated",populateTasks);
+    mc.items.addEventListener("removed",populateTasks);
+}
