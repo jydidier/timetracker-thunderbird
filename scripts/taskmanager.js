78df7501d7587af1bb660904d1429c77f1465674
Jean-Yves Didier <jeanyves.didier@univ-evry.fr>
2025-12-09 10:04:58 +0100

    added a class to manage tasks

---
+++ scripts/taskmanager.js
@@ -0,0 +1,21 @@
+/** this class should manage all that is related to tasks */
+
+class TaskManager {
+    #tasks = new Map();
+    #taskTree = new Map();
+    #calendarId = null;
+
+    constructor() {
+        this.#calendarId = calendarId;
+
+
+    }
+
+
+
+
+
+
+};
+
+export {TaskManager};
